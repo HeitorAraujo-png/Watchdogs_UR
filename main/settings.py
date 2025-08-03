c8c2e877d6a93281982954c919cf7b2750798c9e
Heitor Araujo <heitoraraujodev@gmail.com>
2025-08-03 03:17:02 -0400

    local do arquivo

--- main/settings.py
+++ main/settings.py
@@ -3,5 +3,5 @@ BASE_DIR = os.getcwd()
 DEV_LOG = r'C:\Users\heito\WATCHDOG\log\dev.log'
 ERROR_LOG = r'C:\Users\heito\WATCHDOG\log\error.log'
 ARQUIVO_MONITORADO = r'C:\Users\heito\WATCHDOG\ArquivoMonitorado'
-TEMP = r"C:\Users\heito\WATCHDOG\Processado\CSV_XLSX"
+TEMP = r"C:\Users\heito\WATCHDOG\Processado\CSV"
 FINAL = r"C:\Users\heito\WATCHDOG\Processado\PRONTO"
